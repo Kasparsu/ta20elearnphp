12b3d61b71f263480937636850913db251c17cda
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-10-03 11:45:37 +0300

    Add db connection

--- src/Controllers/PostsController.php
+++ src/Controllers/PostsController.php
@@ -2,10 +2,18 @@
 
 namespace App\Controllers;
 
+use PDO;
+
 class PostsController {
 
     public function index(){
-        echo "some cool posts";
+        $db = new PDO('sqlite:database.sqlite');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $statement = $db->prepare('SELECT * FROM posts;');
+        $statement->execute();
+        //$statement->setFetchMode(PDO::FETCH_ASSOC);
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+        var_dump($results);
     }
 
 }
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,7 +1,39 @@
-<nav>
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/page1">Page 1</a></li>
-        <li><a href="/page2">Page 2</a></li>
-    </ul>
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="/">
+      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+    </a>
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a href="/" class="navbar-item">
+        Home
+      </a>
+
+      <a href="/posts" class="navbar-item">
+        Posts
+      </a>
+
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">
+            Log in
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 </nav>
